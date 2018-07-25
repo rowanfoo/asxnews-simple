f071473bccdd3d726a0da5da80e758588e2843b8
rowanfoo <rowanfoo@gmail.com>
2018-07-25 23:49:28 +0800

    jsp for search web

--- src/main/java/com/selenium/asxnews/controller/AsxNewsController.java
+++ src/main/java/com/selenium/asxnews/controller/AsxNewsController.java
@@ -6,6 +6,7 @@ import com.selenium.asxnews.parser.AsxNewsParser;
 import com.selenium.asxnews.service.ElasticNewsService;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,7 +18,7 @@ import java.util.concurrent.TimeUnit;
 
 
 @RestController
-public class AsxNews {
+public class AsxNewsController {
     @Value( "${selenium.pathfile}" )
     private String seleniumpath;
     @Autowired
@@ -30,7 +31,13 @@ public class AsxNews {
 
     }
 
-    @GetMapping("/news")
+    @GetMapping("/all")
+    public void all() {
+        elasticNewsService.setLoopElasticNews();
+
+    }
+
+        @GetMapping("/news")
     public ArrayList<FundNews> news() {
 
         System.out.println("---------------------file "+ seleniumpath);
@@ -42,12 +49,18 @@ public class AsxNews {
         System.out.println("START  AsxNewsChromeBrowser: ");
         System.setProperty("webdriver.chrome.driver", src.getAbsolutePath());
 
-        WebDriver driver = new ChromeDriver();
+//        WebDriver driver = new ChromeDriver();
+        WebDriver driver;
+
+        ChromeOptions ChromeOptions = new ChromeOptions();
+        ChromeOptions.addArguments("--headless", "window-size=1024,768", "--no-sandbox");
+        driver = new ChromeDriver(ChromeOptions);
+
         String html = "";
         try {
-            String url = "https://www.asx.com.au/asx/statistics/prevBusDayAnns.do"; // today
+           // String url = "https://www.asx.com.au/asx/statistics/prevBusDayAnns.do"; // today
 
-           //String url ="https://www.asx.com.au/asx/statistics/todayAnns.do"; // yesterday
+           String url ="https://www.asx.com.au/asx/statistics/todayAnns.do"; // yesterday
             System.out.println("------URL :" + url);
             driver.get(url);
 
@@ -82,7 +95,7 @@ public class AsxNews {
             driver.quit();
         }
         System.out.println("Run news parser : ");
-        elasticNewsService.setAllElasticNews();
+        elasticNewsService.importElasticNews(true);
         return arr;
 
 
