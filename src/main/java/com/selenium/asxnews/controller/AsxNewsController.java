0d1e5c1bd4ab9cb52a331a6117830d202bfa5a60
rowanfoo <rowanfoo@gmail.com>
2018-10-29 08:29:32 +0800

    clean code

--- src/main/java/com/selenium/asxnews/controller/AsxNewsController.java
+++ src/main/java/com/selenium/asxnews/controller/AsxNewsController.java
@@ -33,88 +33,25 @@ public class AsxNewsController {
 
     }
 
+    /*
+        import to what page
+     */
     @GetMapping("/importpage")
     public void all(@RequestParam int page ) {
         System.out.println("Rimportpage --  : " + page);
         elasticNewsService.importnewsbypage(page );
 
     }
+
+    /*
+        Schedule import by date , or can be manually trigger
+     */
     //@Scheduled(cron = "0 16 15 ? * MON-FRI")
     @GetMapping("/import")
     public String  imports() {
         System.out.println("Run news parser : ");
-     //   elasticNewsService.importElasticNews(true);
         elasticNewsService.importnewsbydate();
         return "running import now ";
     }
-
-        @GetMapping("/news")
-    public ArrayList<FundNews> news() {
-/*
-        System.out.println("---------------------file "+ seleniumpath);
-        File src = new File(seleniumpath);
-        ArrayList<FundNews> arr = new ArrayList<>();
-
-
-
-        System.out.println("START  AsxNewsChromeBrowser: ");
-        System.setProperty("webdriver.chrome.driver", src.getAbsolutePath());
-
-//        WebDriver driver = new ChromeDriver();
-        WebDriver driver;
-
-        ChromeOptions ChromeOptions = new ChromeOptions();
-        ChromeOptions.addArguments("--headless", "window-size=1024,768", "--no-sandbox");
-        driver = new ChromeDriver(ChromeOptions);
-
-        String html = "";
-        try {
-           // String url = "https://www.asx.com.au/asx/statistics/prevBusDayAnns.do"; // today
-
-           String url ="https://www.asx.com.au/asx/statistics/todayAnns.do"; // yesterday
-            System.out.println("------URL :" + url);
-            driver.get(url);
-
-            try {
-                TimeUnit.SECONDS.sleep(20);
-                System.out.println("---------------------getPage ");
-                html = driver.getPageSource();
-                System.out.println("---------------------quit ");
-
-                driver.quit();
-                System.out.println("---------------------parse ");
-
-                arr =  parser.parse(html);
-
-                arr.forEach((a)->{
-
-                    System.out.println("  OBJ : " + a);
-
-                });
-
-
-            } catch (Exception e) {
-                System.out.println("driver page : " + e);
-
-            }
-
-        } catch (Exception e) {
-            System.out.println("------Exception :" + e);
-        }
-
-        finally {
-            driver.quit();
-        }
-        System.out.println("Run news parser : ");
-        elasticNewsService.importElasticNews(true);
-        return arr;
-
-*/
-    return null;
-        //System.out.println("FINISH : ");
-    }
-
-
-
 
 }
